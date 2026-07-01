d14b7c222ccabb7109f0f8f4b767cb57dcdb82f9
RojanShrestha1992 <pheonixeder@gmail.com>
2026-07-01 14:21:24 +0545

    first commit

---
+++ mini-practice/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const fs = require('fs')
+
+app.set("view engine", "ejs");
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get('/', (req, res) => {
+    fs.readdir(`./files`, function(err, files){
+        // console.log(files)
+        res.render('index', {files:files})
+    })
+})
+
+app.post('/create', (req, res) => {
+//    console.log(req.body)
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function(err){
+        if(err){
+            console.log(err)
+        } else {
+            res.redirect('/')
+        }
+})
+
+})
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000')
+})
